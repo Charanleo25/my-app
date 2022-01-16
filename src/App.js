439647ea3b61f01fe2b3c44bc8abf43f6b3677d9
charanleo25 <charanleo25@gmail.com>
2022-01-16 21:42:04 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import TodoList from './ToDoList';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let user = "john"
+const Home = () => {
+    return (
+        <div>
+            <h1>hello from home {user}</h1>
+        </div>
+    )
+}
+
+export default Home;
